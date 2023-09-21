e7d88e822256df8220ec4c3380e25b730ccebd29
Robin Scholz <robin@intmagic.com>
2023-09-21 10:44:05 +0200

    fix(utils): replace uuid with custom implementation

--- packages/utils/src/functions/uuid.ts
+++ packages/utils/src/functions/uuid.ts
@@ -1,7 +1,18 @@
-import crypto from 'crypto'
+// This implementation is meant for internal use only.
+// It is only used to generate a unique IDs for the `key` props.
+// It should not replace crypto.randomUUID() or window.crypto.randomUUID().
 
 export function uuid() {
-  return typeof window !== 'undefined'
-    ? window.crypto.randomUUID()
-    : crypto.randomUUID()
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+    .split('')
+    .reduce(
+      (c, i) =>
+        c +
+        (i === 'x'
+          ? Math.floor(Math.random() * 0xf).toString(16)
+          : i === 'y'
+          ? Math.floor(Math.random() * 4 + 8).toString(16)
+          : i),
+      '',
+    )
 }
