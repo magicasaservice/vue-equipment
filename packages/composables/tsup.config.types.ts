3bffbf9d7f3215c548b8f43f1781d9cf325bd2f5
Robin Scholz <robin@intmagic.com>
2023-12-22 17:55:19 +0100

    fix: exports

--- packages/composables/tsup.config.types.ts
+++ packages/composables/tsup.config.types.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'tsup'
-import { resolve } from 'path'
+import { resolve } from 'node:path'
 
 export default defineConfig({
   entry: [resolve(__dirname, './*/index.ts')],
