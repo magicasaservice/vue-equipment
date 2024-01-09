3d00395cffc570e831d158895522c55c9d662d49
Robin Scholz <robin@intmagic.com>
2024-01-09 16:09:17 +0100

    fix(plugins): drawer nuxt module

---
+++ packages/plugins/MagicDrawer/nuxt.ts
@@ -0,0 +1,28 @@
+import {
+  defineNuxtModule,
+  createResolver,
+  addComponentsDir,
+  addImports,
+} from '@nuxt/kit'
+
+export default defineNuxtModule({
+  meta: {
+    name: '@maas/vue-equipment/nuxt/MagicDrawer',
+  },
+  setup() {
+    const resolver = createResolver(import.meta.url)
+    addComponentsDir({
+      path: resolver.resolve('src/components'),
+      global: true,
+      pathPrefix: false,
+    })
+    addImports({
+      from: '@maas/vue-equipment/plugins/MagicDrawer',
+      name: 'useDrawerApi',
+    })
+    addImports({
+      from: '@maas/vue-equipment/plugins/MagicDrawer',
+      name: 'useDrawerEmitter',
+    })
+  },
+})
