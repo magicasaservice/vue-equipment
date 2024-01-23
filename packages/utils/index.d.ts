7060e4f7dcac22c2da0122e073b5973173e178d4
Robin Scholz <robin@intmagic.com>
2024-01-23 12:49:02 +0100

    fix(utils): types

--- packages/utils/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from './src/functions/clampValue';
-export * from './src/functions/isIOS';
-export * from './src/functions/mapValue';
-export * from './src/functions/uuid';
-export * from './src/functions/uniq';
-export * from './src/functions/slugify';
-export type * from './src/types';
