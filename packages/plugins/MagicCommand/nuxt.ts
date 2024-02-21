2e7dcfe88f2da1a7d39abfe550551dd6f6e05c7c
Robin Scholz <robin@intmagic.com>
2024-02-21 12:35:43 +0100

    wip(plugins): MagicCommand

--- packages/plugins/MagicCommand/nuxt.ts
+++ packages/plugins/MagicCommand/nuxt.ts
@@ -3,7 +3,6 @@ import {
   createResolver,
   addComponentsDir,
   addImports,
-  addComponent,
 } from '@nuxt/kit'
 
 export default defineNuxtModule({
@@ -12,19 +11,18 @@ export default defineNuxtModule({
   },
   setup() {
     const resolver = createResolver(import.meta.url)
-    // addComponentsDir({
-    //   path: resolver.resolve('src/components'),
-    //   global: true,
-    //   pathPrefix: false,
-    // })
-
-    // addImports({
-    //   from: '@maas/vue-equipment/plugins/MagicDrawer',
-    //   name: 'useDrawerApi',
-    // })
-    // addImports({
-    //   from: '@maas/vue-equipment/plugins/MagicDrawer',
-    //   name: 'useDrawerEmitter',
-    // })
+    addComponentsDir({
+      path: resolver.resolve('src/components'),
+      global: true,
+      pathPrefix: false,
+    })
+    addImports({
+      from: '@maas/vue-equipment/plugins/MagicCommand',
+      name: 'useCommandApi',
+    })
+    addImports({
+      from: '@maas/vue-equipment/plugins/MagicCommand',
+      name: 'useCommandEmitter',
+    })
   },
 })
