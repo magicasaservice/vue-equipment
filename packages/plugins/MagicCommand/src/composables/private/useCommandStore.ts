8410dda0401e5eb5b73b683ad957984dc9e733de
Robin Scholz <robin@intmagic.com>
2024-02-23 13:12:38 +0100

    fix(plugins): MagicCommand

--- packages/plugins/MagicCommand/src/composables/private/useCommandStore.ts
+++ packages/plugins/MagicCommand/src/composables/private/useCommandStore.ts
@@ -38,37 +38,37 @@ export function useCommandStore() {
 
     itemElements.forEach((el, index) => {
       const itemId = el.dataset.itemId
-      const item = instance.items.find((item) => item.id === itemId)
+      const item = instance.items?.find((item) => item.id === itemId)
 
       if (item) {
         item.index = index
       }
     })
 
-    instance.items.sort((a, b) => a.index - b.index)
+    instance.items?.sort((a, b) => a.index - b.index)
   }
 
   function addItem(id: string, item: string) {
     const instance = findInstance(id)
-    instance.items.push({ index: -1, id: item })
+    instance.items?.push({ index: -1, id: item })
   }
 
   function removeItem(id: string, item: string) {
     const instance = findInstance(id)
     if (instance) {
-      instance.items = instance.items.filter((x) => x.id !== item)
+      instance.items = instance.items?.filter((x) => x.id !== item)
     }
   }
 
   function addView(id: string, view: string) {
     const instance = findInstance(id)
-    instance.views.push(view)
+    instance.views?.push(view)
   }
 
   function removeView(id: string, view: string) {
     const instance = findInstance(id)
     if (instance) {
-      instance.views = instance.views.filter((x: string) => x !== view)
+      instance.views = instance.views?.filter((x: string) => x !== view)
     }
   }
 
