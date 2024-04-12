d1b88513d458746cc4afde8d07a8f2d980a486e1
Robin Scholz <robin@intmagic.com>
2024-04-12 09:37:24 +0200

    feat(docs): auto import all plugins

--- apps/docs/.vitepress/theme/index.ts
+++ apps/docs/.vitepress/theme/index.ts
@@ -1,31 +1,18 @@
 import DefaultTheme from 'vitepress/theme'
 import Layout from './Layout.vue'
-import {
-  MagicCommandPlugin,
-  MagicCookiePlugin,
-  MagicDrawerPlugin,
-  MagicMarqueePlugin,
-  MagicModalPlugin,
-  MagicNoisePlugin,
-  MagicPlayerPlugin,
-  MagicScrollPlugin,
-  MagicToastPlugin,
-} from '@maas/vue-equipment/plugins'
+import * as VueEquipmentPlugins from '@maas/vue-equipment/plugins'
 
 import 'virtual:uno.css'
 
 export default {
   extends: DefaultTheme,
   Layout,
-  enhanceApp({ app }) {
-    app.use(MagicCommandPlugin)
-    app.use(MagicCookiePlugin)
-    app.use(MagicDrawerPlugin)
-    app.use(MagicMarqueePlugin)
-    app.use(MagicModalPlugin)
-    app.use(MagicNoisePlugin)
-    app.use(MagicPlayerPlugin)
-    app.use(MagicScrollPlugin)
-    app.use(MagicToastPlugin)
+  async enhanceApp({ app }) {
+    for (const plugin in VueEquipmentPlugins) {
+      // Filter out composables, symbols, etc
+      if (plugin.includes('Plugin')) {
+        app.use(VueEquipmentPlugins[plugin])
+      }
+    }
   },
 }
