86a5d1bb78c319b4b7711ffc4282fa2a89790db0
Robin Scholz <robin@intmagic.com>
2024-05-06 10:08:28 +0200

    wip(plugins): MagicDraggable > demo

--- packages/plugins/MagicDraggable/demo.vue
+++ packages/plugins/MagicDraggable/demo.vue
@@ -1,23 +1,46 @@
 <template>
-  <magic-draggable
-    id="demo-draggable"
-    :options="{
-      snapPoints: [
-        'top-left',
-        // 'top-center',
-        'top-right',
-        // 'center-left',
-        // 'center',
-        // 'center-right',
-        'bottom-left',
-        // 'bottom-center',
-        'bottom-right',
-      ],
-      initial: {
-        snapPoint: 'top-left',
-      },
-    }"
-  >
-    <div class="bg-white aspect-square w-40 rounded-md" />
-  </magic-draggable>
+  <div>
+    <magic-draggable
+      id="demo-draggable"
+      :options="{
+        snapPoints: [
+          'top-left',
+          // 'top-center',
+          'top-right',
+          // 'center-left',
+          // 'center',
+          // 'center-right',
+          'bottom-left',
+          // 'bottom-center',
+          'bottom-right',
+        ],
+        initial: {
+          snapPoint: 'top-left',
+        },
+      }"
+    >
+      <div class="bg-[blue] aspect-square w-40 rounded-md" />
+    </magic-draggable>
+    <magic-draggable
+      id="demo-contained-draggable"
+      :options="{
+        snapPoints: [
+          'top-center',
+          'center-left',
+          'center-right',
+          'bottom-center',
+        ],
+        initial: { snapPoint: 'top-center' },
+      }"
+      class="border-solid border-2 border-gray-500 aspect-square"
+    >
+      <div class="bg-[yellow] aspect-square w-20 rounded-full" />
+    </magic-draggable>
+  </div>
 </template>
+
+<style>
+#demo-contained-draggable {
+  --magic-draggable-position: relative;
+}
+</style>
