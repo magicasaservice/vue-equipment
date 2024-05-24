b322576ba20bca66944e0b52bf2f64e36c6e1e0f
Robin Scholz <robin@intmagic.com>
2024-05-24 10:07:53 +0200

    fix(plugins): symbol export

--- packages/plugins/MagicCommand/src/symbols/index.ts
+++ packages/plugins/MagicCommand/src/symbols/index.ts
@@ -1,5 +1,5 @@
 import type { InjectionKey, MaybeRef } from 'vue'
-import type { MagicCommandOptions as MagicCommandOptions } from '../types'
+import type { MagicCommandOptions } from '../types'
 
 const MagicCommandInstanceId = Symbol() as InjectionKey<MaybeRef<string>>
 const MagicCommandProviderOptions =
