9bb9721ccae7d9a3f3f29eaecfb67c50e7540467
Robin Scholz <robin@intmagic.com>
2025-02-04 08:21:28 +0100

    wip: MagicToast > refactor

--- packages/plugins/MagicToast/demo/DemoToast.vue
+++ packages/plugins/MagicToast/demo/DemoToast.vue
@@ -1,19 +1,20 @@
 <template>
   <div
-    class="bg-surface-elevation-base text-surface type-label-text-xl rounded-[1rem] p-2 w-[300px] pointer-events-auto cursor-pointer flex justify-between items-center"
+    class="bg-surface-elevation-base text-surface type-label-text-lg rounded-[1.375rem] p-2 pl-4 w-[320px] pointer-events-auto cursor-pointer flex justify-between items-center"
   >
     <span>{{ message }}</span>
-    <m-button size="sm" @click.stop="emit('remove')"> Close </m-button>
+    <m-button size="sm" @click.stop="emit('remove')">Close</m-button>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { MButton } from '@maas/mirror/vue'
 
-interface Props {
+interface DemoToastProps {
   message: string
 }
-
-const emit = defineEmits(['remove'])
-defineProps<Props>()
+defineProps<DemoToastProps>()
+const emit = defineEmits<{
+  (e: 'remove'): void
+}>()
 </script>
