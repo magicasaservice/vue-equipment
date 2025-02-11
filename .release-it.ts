bbd8bf3bbb74f5d748f77a4484dfd67adb5eb6eb
Robin Scholz <robin@intmagic.com>
2025-02-11 16:03:26 +0100

    chore: release-it settings

---
+++ .release-it.ts
@@ -0,0 +1,27 @@
+import type { Config } from 'release-it'
+
+export default {
+  git: {
+    commit: true,
+    tag: true,
+    push: true,
+    commitMessage: 'chore: release v${version}',
+    requireCleanWorkingDir: true,
+    requireBranch: 'main',
+  },
+  github: {
+    release: true,
+    autoGenerate: true,
+    preRelease: true, // TODO: Remove this for the stable release
+  },
+  npm: {
+    allowSameVersion: true,
+    tag: 'beta', // TODO: Remove this for the stable release
+  },
+  plugins: {
+    '@release-it/bumper': {
+      in: 'package.json',
+      out: ['package.json', 'packages/*/package.json', 'apps/*/package.json'],
+    },
+  },
+} satisfies Config
