a89ca7703d7aecfff4c566c21e87d14f6faadb02
Robin Scholz <robin@intmagic.com>
2025-02-13 08:15:48 +0100

    chore: nuxt > playground > config

--- packages/nuxt/playground/nuxt.config.ts
+++ packages/nuxt/playground/nuxt.config.ts
@@ -3,6 +3,8 @@ import { kebabCase } from 'scule'
 
 import { composables, plugins } from '../../metadata'
 
+const dist = '../../../dist'
+
 export default defineNuxtConfig({
   modules: [
     '@maas/vue-equipment/nuxt',
@@ -29,22 +31,23 @@ export default defineNuxtConfig({
     viewer: false,
   },
 
+
   alias: {
     '@maas/mirror/tokens': path.resolve(__dirname, '../../../.maas/tokens/css'),
-    '@maas/vue-equipment/nuxt': path.resolve(__dirname, '../src/module'),
-    '@maas/vue-equipment/composables': path.resolve(
+    '@maas/vue-equipment/nuxt': path.resolve(
       __dirname,
-      '../../../dist/composables'
+      `../../nuxt/src/module`
     ),
     '@maas/vue-equipment/plugins/MagicPlayer/css': path.resolve(
       __dirname,
-      '../../../dist/plugins/MagicPlayer/src/css'
+      `${dist}/plugins/MagicPlayer/src/css`
     ),
-    '@maas/vue-equipment/plugins': path.resolve(
+    '@maas/vue-equipment/plugins': path.resolve(__dirname, `${dist}/plugins`),
+    '@maas/vue-equipment/utils': path.resolve(__dirname, `${dist}/utils`),
+    '@maas/vue-equipment/composables': path.resolve(
       __dirname,
-      '../../../dist/plugins'
+      `${dist}/composables`
     ),
-    '@maas/vue-equipment/utils': path.resolve(__dirname, '../../../dist/utils'),
   },
 
   css: [
