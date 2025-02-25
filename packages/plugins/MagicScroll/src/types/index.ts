5038db02c19a8e08eefc531a5244085bcd034fbf
Robin Scholz <robin@intmagic.com>
2025-02-25 18:36:55 +0100

    fix: MagicScroll > types (regression)

--- packages/plugins/MagicScroll/src/types/index.ts
+++ packages/plugins/MagicScroll/src/types/index.ts
@@ -5,13 +5,13 @@ export type CollisionEdge = 'top' | 'bottom'
 
 export type ScrollIntersection =
   | 'top-top'
-  | 'top'
+  | 'top-center'
   | 'top-bottom'
   | 'center-top'
   | 'center-center'
   | 'center-bottom'
   | 'bottom-top'
-  | 'bottom'
+  | 'bottom-center'
   | 'bottom-bottom'
 
 export interface CollisionOffset {
