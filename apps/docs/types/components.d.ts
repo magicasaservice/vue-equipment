50d9107a22ecaa9a148bc337dcb89adac9db1212
Robin Scholz <robin@intmagic.com>
2025-08-22 10:53:11 +0200

    chore: types

--- apps/docs/types/components.d.ts
+++ apps/docs/types/components.d.ts
@@ -14,7 +14,6 @@ declare module 'vue' {
     IMaasBrandNuxt500: typeof import('~icons/maas/brand-nuxt500')['default']
     IMaasBrandVue500: typeof import('~icons/maas/brand-vue500')['default']
     IMaasCarabiner500: typeof import('~icons/maas/carabiner500')['default']
-    IMaasCheck500: typeof import('~icons/maas/check500')['default']
     IMaasChevronE500: typeof import('~icons/maas/chevron-e500')['default']
     IMaasChevronW500: typeof import('~icons/maas/chevron-w500')['default']
     IMaasClose500: typeof import('~icons/maas/close500')['default']
@@ -29,7 +28,6 @@ declare module 'vue' {
     IMaasMaasVe500: typeof import('~icons/maas/maas-ve500')['default']
     IMaasMoon500: typeof import('~icons/maas/moon500')['default']
     IMaasSignInfoOval500: typeof import('~icons/maas/sign-info-oval500')['default']
-    IMaasSpacer500: typeof import('~icons/maas/spacer500')['default']
     IMaasSun500: typeof import('~icons/maas/sun500')['default']
   }
 }
