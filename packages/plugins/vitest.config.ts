b9a5f9904c5af436d022bcc801acfafd1ec2e46b
Robin Scholz <robin@intmagic.com>
2025-08-27 12:50:58 +0200

    fix: add alias to vitest config

--- packages/plugins/vitest.config.ts
+++ packages/plugins/vitest.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vitest/config'
+import { resolve } from 'node:path'
 import vue from '@vitejs/plugin-vue'
+
+import { plugins } from '../../packages/metadata'
 
 export default defineConfig({
   plugins: [vue()],
@@ -7,5 +10,32 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     include: ['**/test/**/*.spec.ts'],
+  },
+  resolve: {
+    alias: [
+      ...plugins.map((plugin) => {
+        return {
+          find: `@maas/vue-equipment/plugins/${plugin.name}/css`,
+          replacement: resolve(__dirname, `/${plugin.name}/src/css`),
+        }
+      }),
+      {
+        find: '@maas/vue-equipment/plugins',
+        replacement: resolve(__dirname),
+      },
+      {
+        find: '@maas/vue-equipment/composables',
+        replacement: resolve(__dirname, '../composables'),
+      },
+      {
+        find: '@maas/vue-equipment/utils/css',
+        replacement: resolve(__dirname, '../utils/src/css'),
+      },
+      {
+        find: '@maas/vue-equipment/utils',
+        replacement: resolve(__dirname, '../utils'),
+      },
+    ],
+    dedupe: ['vue'],
   },
 })
